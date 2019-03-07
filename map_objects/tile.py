5f0fb779225fd894633ea8b1d641cec1ce15788f
Mike <chunkomike@gmail.com>
2019-03-06 22:44:29 -0500

    Fixed some bugs

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -4,12 +4,13 @@
 
 # This file contains the class representing a tile on a map
 
+
 class Tile:
     def __init__(self, blocked, block_sight=None):
         self.blocked = blocked
 
         # By default, a blocked tile also blocks sight
         if block_sight is None:
-            block_sight = blocked
+            self.block_sight = blocked
         else:
-            self.block_sight = block_sight
+            self.block_sight = blocked
