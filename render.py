88133ac85f5bb23084495158f8f8b6f059a24e1d
Mike <chunkomike@gmail.com>
2019-03-25 11:11:36 -0400

    Slightly restructured render_all

--- render.py
+++ render.py
@@ -10,6 +10,30 @@ from engine import colors
 
 def render_all(con, entities, game_map, fov_map, fov_recalculate, screen_width, screen_height):
     # Draw the map
+    draw_map(con, game_map, fov_map, fov_recalculate)
+
+    # Draw all entities
+    for entity in entities:
+        draw_entity(con, entity, fov_map)
+
+    # Draw the player stats
+    tcod.console_set_default_foreground(con, tcod.white)
+    tcod.console_print_ex(con, 1, screen_height - 4, tcod.BKGND_NONE, tcod.LEFT,
+                          "PWR: {0}".format(entities[0].power))
+    tcod.console_print_ex(con, 1, screen_height - 3, tcod.BKGND_NONE, tcod.LEFT,
+                          "DEF: {0}".format(entities[0].defense))
+    tcod.console_print_ex(con, 1, screen_height - 2, tcod.BKGND_NONE, tcod.LEFT,
+                          "HP : {0:02}/{1:02}".format(entities[0].hp, entities[0].max_hp))
+
+    tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+
+def clear_all(con, entities):
+    for entity in entities:
+        clear_entity(con, entity)
+
+
+def draw_map(con, game_map, fov_map, fov_recalculate):
     if fov_recalculate:
         for y in range(game_map.height):
             for x in range(game_map.width):
@@ -31,25 +55,6 @@ def render_all(con, entities, game_map, fov_map, fov_recalculate, screen_width, 
                         tcod.console_set_char_background(con, x, y, colors.get("dark wall"), tcod.BKGND_SET)
                     else:
                         tcod.console_set_char_background(con, x, y, colors.get("dark ground"), tcod.BKGND_SET)
-
-    # Draw all entities
-    for entity in entities:
-        draw_entity(con, entity, fov_map)
-
-    tcod.console_set_default_foreground(con, tcod.white)
-    tcod.console_print_ex(con, 1, screen_height - 4, tcod.BKGND_NONE, tcod.LEFT,
-                          "PWR: {0}".format(entities[0].power))
-    tcod.console_print_ex(con, 1, screen_height - 3, tcod.BKGND_NONE, tcod.LEFT,
-                          "DEF: {0}".format(entities[0].defense))
-    tcod.console_print_ex(con, 1, screen_height - 2, tcod.BKGND_NONE, tcod.LEFT,
-                          "HP : {0:02}/{1:02}".format(entities[0].hp, entities[0].max_hp))
-
-    tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
-
-
-def clear_all(con, entities):
-    for entity in entities:
-        clear_entity(con, entity)
 
 
 def draw_entity(con, entity, fov_map):
